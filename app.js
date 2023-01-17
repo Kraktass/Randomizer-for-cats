9d574f94800e354cd1a32008330090bc0050a057
Jakob Sjöholm <jakob.sjoholm@live.se>
2023-01-17 15:52:36 +0100

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var url = "https://api.thecatapi.com/v1/images/search?limit=10";
-var imgElement = document.getElementsByTagName('img')[0];
+var imageElement = document.createElement("img");
+imageElement.className = ("catImage");
 var p = fetch(url)
     .then(function (response) {
     return response.json();
@@ -7,9 +8,15 @@ var p = fetch(url)
     displayCat(catsArray);
 });
 function displayCat(cats) {
-    console.log(cats[0].url);
-    var catImage = cats[0].url;
-    console.log(catImage);
-    imgElement.setAttribute('src', catImage);
+    var randomizeBtn = document.querySelector(".randomizeButton");
+    randomizeBtn.addEventListener("click", function (e) {
+        var randomNr = Math.floor(Math.random() * cats.length);
+        var catsDiv = document.querySelector(".container");
+        catsDiv.innerHTML = "";
+        catsDiv.style.display = "block";
+        imageElement.style.height = "500px";
+        imageElement.style.width = "850px";
+        imageElement.src = cats[randomNr].url;
+        catsDiv.append(imageElement);
+    });
 }
-;
--- app.ts
+++ app.ts
@@ -1,16 +1,26 @@
 const url = "https://api.thecatapi.com/v1/images/search?limit=10";
-const imgElement = document.getElementsByTagName('img')[0];
+const imageElement = document.createElement("img") as HTMLImageElement;
+imageElement.className = ("catImage");
+
 const p = fetch(url)
-    .then(response => {
-        return response.json();
+.then(response => {
+   return response.json();
 }).then(catsArray => {
-    displayCat(catsArray);
+   displayCat(catsArray);
 });
 
-function displayCat (cats: any) {
-    console.log(cats[0].url);
-    let catImage = cats[0].url
-    console.log(catImage);
-    imgElement.setAttribute('src', catImage);
-};
 
+function displayCat(cats: any){
+   let randomizeBtn = document.querySelector(".randomizeButton") as HTMLButtonElement;
+   randomizeBtn.addEventListener("click", (e) => {
+        let randomNr = Math.floor(Math.random() * cats.length);
+        const catsDiv = document.querySelector(".container") as HTMLElement;
+        catsDiv.innerHTML = "";
+        catsDiv.style.display = "block";
+        imageElement.style.height = "500px"
+        imageElement.style.width = "850px"
+        imageElement.src = cats[randomNr].url;
+        catsDiv.append(imageElement);
+   })
+
+}
